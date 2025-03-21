1eb3684e8b51a3519843c5bfc9491968f6a4583c
Kellen-Linse <kblinse06@gmail.com>
2025-03-21 14:41:43 -0700

    basic functionality

--- main.ts
+++ main.ts
@@ -1,134 +1,114 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile, Notice, TFolder } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class KanbanSyncPlugin extends Plugin {
+	private readonly folderPath = "Sources/synced-kanbans";
+	private readonly mainKanbanFileName = "main.md";
 
 	async onload() {
-		await this.loadSettings();
+		console.log("Kanban Sync Plugin loaded.");
+		this.syncKanbans();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.app.workspace.on("active-leaf-change", async () => {
+			const activeFile = this.app.workspace.getActiveFile();
+			if (
+				activeFile &&
+				activeFile.path ===
+					`${this.folderPath}/${this.mainKanbanFileName}`
+			) {
+				await this.syncKanbans();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+	}
+
+	async onunload() {
+		console.log("Kanban Sync Plugin unloaded.");
+	}
+
+	async syncKanbans() {
+		try {
+			const folder = this.app.vault.getAbstractFileByPath(
+				this.folderPath
+			);
+			if (!folder || !(folder instanceof TFolder)) {
+				new Notice(`Folder "${this.folderPath}" does not exist.`);
+				return;
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			// Get all files in the folder
+			const files = this.app.vault
+				.getFiles()
+				.filter(
+					(file) =>
+						file.path.startsWith(this.folderPath) &&
+						file.name !== this.mainKanbanFileName
+				);
+
+			// Initialize task sets
+			const todoTasks = new Set<string>();
+			const inProgressTasks = new Set<string>();
+			const doneTasks = new Set<string>();
+
+			// Process each Kanban file
+			for (const file of files) {
+				const content = await this.app.vault.read(file);
+
+				// Extract tasks by section
+				this.extractTasks(content, "## To Do", todoTasks);
+				this.extractTasks(content, "## In Progress", inProgressTasks);
+				this.extractTasks(content, "## Done", doneTasks);
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			// Create or update the main Kanban file
+			const mainFile = this.app.vault.getAbstractFileByPath(
+				`${this.folderPath}/${this.mainKanbanFileName}`
+			) as TFile;
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+			const newContent = [
+				"---",
+				"kanban-plugin: board",
+				"---",
+				"",
+				"## To Do",
+				...todoTasks,
+				"",
+				"## In Progress",
+				...inProgressTasks,
+				"",
+				"## Done",
+				...doneTasks,
+				"",
+			].join("\n");
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			if (mainFile) {
+				await this.app.vault.modify(mainFile, newContent);
+			} else {
+				await this.app.vault.create(
+					`${this.folderPath}/${this.mainKanbanFileName}`,
+					newContent
+				);
+			}
+
+			new Notice("Kanban synced successfully!");
+		} catch (error) {
+			console.error("Error syncing Kanban:", error);
+			new Notice(`Error syncing Kanban: ${error.message}`);
+		}
 	}
 
-	onunload() {
+	extractTasks(content: string, sectionHeader: string, taskSet: Set<string>) {
+		let section: RegExpMatchArray | null;
 
-	}
+		if (sectionHeader === "## Done") {
+			section = content.match(/^##\s*Done\s*([\s\S]*?)(?=^##|$)/m);
+		} else {
+			section = content.match(
+				new RegExp(`^${sectionHeader}[\\s\\S]*?(?=^##|\\Z)`, "m")
+			);
+		}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		if (section) {
+			const tasks = section[0].match(/^- .+$/gm);
+			if (tasks) tasks.forEach((task) => taskSet.add(task));
+		}
 	}
 }
